67333beadf24425b8ccedaf6bd7acfe09595bef1
blackPavlin <boikiv.bogdan@gmail.com>
2022-02-25 21:06:18 +0400

    Добавил метатеги

--- client/src/views/Calculator.vue
+++ client/src/views/Calculator.vue
@@ -36,24 +36,25 @@
           </div>
         </div>
 
-        <div class="mb-5">
-          <label class="block text-sm font-medium text-gray-700"
-            >ROI в годовых</label
+        <div class="text-left">
+          <div
+            class="mt-5 text-xl font-medium text-gray-900 flex justify-between"
           >
-
-          <div class="mt-1 relative rounded-md shadow-sm">
-            <input
-              type="text"
-              value="95 %"
-              class="mt-1 block w-full rounded-md bg-gray-100 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              disabled
-            />
+            <span>Всего интвестированно: </span>
+            <span class="font-bold">{{ totalInvestment.toFixed(0) }}</span>
           </div>
-        </div>
 
-        <div class="text-center">
-          <div class="mt-5 text-xl font-bold text-gray-900">
-            Итоговая сумма: {{ summ.toFixed() }}
+          <div
+            class="mt-2 text-xl font-medium text-gray-900 flex justify-between"
+          >
+            Итоговая сумма:
+            <span class="font-bold">{{ balance.toFixed(0) }}</span>
+          </div>
+
+          <div
+            class="mt-2 text-xl font-medium text-gray-900 flex justify-between"
+          >
+            Доход: <span class="font-bold">{{ profit.toFixed(0) }}</span>
           </div>
         </div>
       </form>
@@ -68,15 +69,36 @@ export default defineComponent({
   name: "Calculator",
   setup() {
     const form = reactive({
-      amount: 100000,
+      amount: 50000,
       years: 1,
     });
 
-    const summ = computed(() => form.amount * Math.pow(0.95 + 1, form.years));
+    // Всего инвестированно
+    const totalInvestment = computed(() => form.amount * form.years * 12);
+
+    // Всего на балансе
+    const balance = computed(() => {
+      const incomePerQuarters: number[] = [];
+
+      for (let i = 0; i < (form.years * 12) / 4; i += 1) {
+        const investments =
+          form.amount * 4 +
+          incomePerQuarters.reduce((prev, curr) => prev + curr * 0.43, 0);
+
+        incomePerQuarters.push(investments);
+      }
+
+      return incomePerQuarters.reduce((prev, curr) => prev + curr, 0);
+    });
+
+    // Доход за период
+    const profit = computed(() => balance.value - totalInvestment.value);
 
     return {
       form,
-      summ,
+      totalInvestment,
+      balance,
+      profit,
     };
   },
 });
